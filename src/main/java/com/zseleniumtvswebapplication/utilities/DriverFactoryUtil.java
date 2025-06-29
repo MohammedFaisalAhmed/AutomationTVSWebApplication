5507372224623887b6f1e521f170c9b75f132258
MohammedFaisalAhmed <faisalahh23@gmail.com>
2025-06-29 23:32:13 +0530

    added DriverFactoryUtil Code

--- src/main/java/com/zseleniumtvswebapplication/utilities/DriverFactoryUtil.java
+++ src/main/java/com/zseleniumtvswebapplication/utilities/DriverFactoryUtil.java
@@ -1,5 +1,51 @@
 package com.zseleniumtvswebapplication.utilities;
 
-public class DriverFactoryUtil {
+import javax.management.RuntimeErrorException;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class DriverFactoryUtil 
+{
+
+	final static ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
+	
+	public static WebDriver getDriver(String browser)
+	{
+		if(driver.get()==null)
+		{
+			switch(browser)
+			{
+			case "Chrome":
+			{
+				driver.set(new ChromeDriver());
+				break;
+			}
+			case "Edge":
+			{
+				driver.set(new EdgeDriver());
+				break;
+			}
+			
+			default: 
+				throw new RuntimeException("no browser");
+			
+			}
+			
+			driver.get().manage().window().maximize();
+		}
+		return driver.get();
+				
+	}
+	
+	public static void closeDriver()
+	{
+		
+		if(driver.get()!=null)
+		{
+			
+			driver.get().quit();
+		}
+	}
 }
