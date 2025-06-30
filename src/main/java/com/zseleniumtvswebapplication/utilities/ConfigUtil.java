d992fbe21c81c2214bfe53529db9e64b196ad252
ZeeshanMallik <zeeshan.sdet@gmail.com>
2025-06-30 15:34:05 +0530

    2nd commit

--- src/main/java/com/zseleniumtvswebapplication/utilities/ConfigUtil.java
+++ src/main/java/com/zseleniumtvswebapplication/utilities/ConfigUtil.java
@@ -3,3 +3,8 @@ package com.zseleniumtvswebapplication.utilities;
 public class ConfigUtil {
 
 }
+/*
+<properties>
+    <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+    <maven.compiler.release>17</maven.compiler.release>
+  </properties>*/
